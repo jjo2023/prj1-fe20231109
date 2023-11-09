b5841cbb7e22b5ef0006c6cc92297f38952fbece
jjo2023 <snowman7685@gmail.com>
2023-11-09 16:18:09 +0900

    Spinner를 테이블 밖으로 뺌

--- src/page/BoardList.js
+++ src/page/BoardList.js
@@ -1,3 +1,63 @@
+import {
+  Box,
+  Spinner,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 export function BoardList() {
-  return <h1>게시물 목록</h1>;
+  const [boardList, setBoardList] = useState(null);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get("/api/board/list")
+      .then((response) => setBoardList(response.data));
+  }, []);
+
+  if (boardList === null){
+    return <Spinner/>
+  }
+
+  return (
+    <Box>
+      <h1>게시물 목록</h1>
+      <Box>
+        <Table>
+          <Thead>
+            <Tr>
+              <Th>id</Th>
+              <Th>title</Th>
+              <Th>by</Th>
+              <Th>at</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {
+              boardList.map((board) => (
+                <Tr
+                  _hover={{ cursor: "pointer" }}
+                  key={board.id}
+                  onClick={() => navigate("/board/" + board.id)}
+                >
+                  <Td>{board.id}</Td>
+                  <Td>{board.title}</Td>
+                  <Td>{board.writer}</Td>
+                  <Td>{board.inserted}</Td>
+                </Tr>
+              ))
+            }
+          </Tbody>
+        </Table>
+      </Box>
+    </Box>
+  );
 }
