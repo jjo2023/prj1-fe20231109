9d6058f4f2e611770ce4e4106f4c77b4af7977f2
jjo2023 <snowman7685@gmail.com>
2023-11-14 11:39:39 +0900

    글 수정시 writer Input 삭제

--- src/page/board/BoardEdit.js
+++ src/page/board/BoardEdit.js
@@ -96,18 +96,7 @@ export function BoardEdit() {
           }}
         />
       </FormControl>
-      <FormControl>
-        <FormLabel>작성자</FormLabel>
-        <Input
-          background={"pink.50"}
-          value={board.writer}
-          onChange={(e) => {
-            updateBoard((draft) => {
-              draft.writer = e.target.value;
-            });
-          }}
-        />
-      </FormControl>
+
       <Button colorScheme="blue" onClick={onOpen}>
         저장
       </Button>
